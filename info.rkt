ca0e815a4cd36f0bf75745622e6459eebbb29dde
Nick Breitling <breitling.nw@gmail.com>
2026-04-07 01:06:36 -0500

    info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "stupid-agenda")
+(define version 1.0)
+(define deps '("base"
+               "csv-reading"
+               "srfi"))
+(define distribution-preference 'binary)
